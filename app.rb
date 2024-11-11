f887ca9bc9c191162dd39624b0b6098bdba77c22
Vincent Thomas <77443389+vincent-thomas@users.noreply.github.com>
2024-11-11 14:45:02 +0100

    la till CRUD

--- app.rb
+++ app.rb
@@ -1,7 +1,82 @@
 class App < Sinatra::Base
+    def db
+      return @db if @db
 
-    get '/' do
-        erb(:"index")
+      @db = SQLite3::Database.new("db/fruits.sqlite")
+      @db.results_as_hash = true
+
+      return @db
+    end
+
+    get '/lists' do
+      @lists = db.execute("SELECT * FROM todolists")
+      erb(:"lists")
+    end
+    post '/lists' do
+
+      @lists = db.execute("INSERT INTO todolists (name) VALUES (?)", [params["name"]])
+      redirect "/lists"
+    end
+
+    post '/lists/:list_id/delete' do |list_id|
+
+      db.execute("DELETE FROM todolists WHERE id = ?", [list_id])
+      redirect "/lists"
+    end
+
+    post '/lists/:list_id/todos' do |list_id|
+      name = params["name"]
+
+      db.execute("INSERT INTO todos (name, todolist_id) VALUES (?, ?)", [name, list_id])
+      redirect "/lists/#{list_id}/todos"
+    end
+
+    get '/lists/:list_id/todos' do |list_id|
+      name = params["name"]
+
+      @list_id = list_id
+
+      @todos = db.execute('SELECT * from todos WHERE todolist_id = ?', [list_id])
+      erb(:"todos")
+    end
+    get '/lists/:list_id/todos/:todo_id' do | list_id, todo_id |
+      list = db.execute("SELECT * FROM todolists WHERE id = ?", [list_id.to_i])
+      todos = db.execute('SELECT * FROM todos WHERE id = ?', [todo_id.to_i])
+
+      if list.length == 0
+        redirect "/lists"
+      end
+
+      if todos.length == 0
+        redirect "/lists/#{list_id}/todos"
+      end
+
+      @todo = todos[0]
+      @list = list[0]
+
+      erb(:"one_todo")
+    end
+
+    post '/lists/:list_id/todos/:todo_id/delete' do | list_id, todo_id |
+      @list_id = list_id
+      @todo = db.execute('DELETE FROM todos WHERE id = ?', [todo_id])
+      redirect "/lists/#{list_id}/todos"
+    end
+
+    post '/lists/:list_id/todos/:todo_id/update' do | list_id, todo_id |
+      @list_id = list_id
+      @todo = db.execute('UPDATE todos SET name = ? WHERE id = ?', [params["name"], todo_id])
+      redirect "/lists/#{list_id}/todos/#{todo_id}"
+    end
+
+    post '/api/account' do
+
+      email = params["email"]
+      password = params["password"]
+
+      p email, password
+
+
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -3,9 +3,62 @@ require 'sqlite3'
 class Seeder
 
   def self.seed!
-    p "doit"
+    drop_tables
+    create_tables
+    populate_tables
   end
 
+  def self.drop_tables
+    db.execute('DROP TABLE IF EXISTS users')
+    db.execute('DROP TABLE IF EXISTS todolists')
+    db.execute('DROP TABLE IF EXISTS todos')
+  end
+
+  def self.create_tables
+
+    db.execute('CREATE TABLE IF NOT EXISTS todolists (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                created_at DATETIME default current_timestamp
+    )')
+
+    db.execute('CREATE TABLE IF NOT EXISTS todos (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                todolist_id INTEGER NOT NULL REFERENCES todolists(id),
+                name TEXT NOT NULL,
+                done_at INTEGER,
+                priority INTEGER,
+                deadline_at INTEGER,
+                created_at DATETIME default current_timestamp
+    )')
+
+    db.execute('CREATE TABLE IF NOT EXISTS tags (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                thing_type TEXT NOT NULL,
+                thing_id TEXT NOT NULL,
+                name TEXT NOT NULL,
+                done_at INTEGER,
+                priority INTEGER,
+                deadline_at INTEGER,
+                created_at DATETIME default current_timestamp
+    )')
+  end
+
+  def self.populate_tables
+    # db.execute('INSERT INTO fruits (name, tastiness, description) VALUES ("Äpple",   7, "En rund frukt som finns i många olika färger.")')
+    # db.execute('INSERT INTO fruits (name, tastiness, description) VALUES ("Päron",    6, "En nästan rund, men lite avläng, frukt. Oftast mjukt fruktkött.")')
+    # db.execute('INSERT INTO fruits (name, tastiness, description) VALUES ("Banan",  4, "En avlång gul frukt.")')
+    # db.execute('INSERT INTO fruits (name, tastiness, description) VALUES ("Mango",  9, "En god (kanske) frukt med jobbig kärna i mitten.")')
+  end
+
+  private
+  def self.db
+    return @db if @db
+    @db = SQLite3::Database.new('db/fruits.sqlite')
+    @db.results_as_hash = true
+    @db
+  end
 end
+
 
 Seeder.seed!
